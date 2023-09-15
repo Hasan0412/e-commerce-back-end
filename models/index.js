6eb1e86f786723bdcfbae12b4e135ec13a708713
Hasan Malik <hmalik0412@gmail.com>
2023-09-15 02:54:32 +0100

    set up all models

--- models/index.js
+++ models/index.js
@@ -5,7 +5,10 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Categories have many Products
 Category.hasMany(Product);
@@ -13,11 +16,15 @@ Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
+  sourceKey: 'id',
+  foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
+  sourceKey: 'id',
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
